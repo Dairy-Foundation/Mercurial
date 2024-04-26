0a0a27cb9be434eb995ce970194212d5c9302f0d
Froze-N-Milk <oscar.chevalier@gmail.com>
2024-04-27 03:02:49 +1000

    feat: SDKSubsystem
    - nice support for using telemetry and hardware map directly from the
      current opmode

---
+++ src/main/kotlin/dev/frozenmilk/mercurial/subsystems/SDKSubsystem.kt
@@ -0,0 +1,22 @@
+package dev.frozenmilk.mercurial.subsystems
+
+import com.qualcomm.robotcore.hardware.HardwareMap
+import dev.frozenmilk.dairy.core.FeatureRegistrar
+import org.firstinspires.ftc.robotcore.external.Telemetry
+
+@Suppress("unused")
+abstract class SDKSubsystem : Subsystem {
+	private val hardwareMapCell: SubsystemObjectCell<HardwareMap> by lazy {
+		SubsystemObjectCell(this) {
+			FeatureRegistrar.activeOpMode.hardwareMap
+		}
+	}
+	protected val hardwareMap by hardwareMapCell
+
+	private val telemetryCell: SubsystemObjectCell<Telemetry> by lazy {
+		SubsystemObjectCell(this) {
+			FeatureRegistrar.activeOpMode.telemetry
+		}
+	}
+	protected val telemetry by telemetryCell
+}
