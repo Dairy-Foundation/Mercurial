9602f22ad8fe0d1661b474e3af1f2f432c86de40
Oscar Chevalier <oscar.chevalier@gmail.com>
2024-12-31 02:47:19 +1100

    chore: version bump to 1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,7 +72,7 @@ publishing {
 		register<MavenPublication>("release") {
 			groupId = "dev.frozenmilk.mercurial"
 			artifactId = "Mercurial"
-			version = "1.0.0"
+			version = "1.0.1"
 
 			afterEvaluate {
 				from(components["release"])
