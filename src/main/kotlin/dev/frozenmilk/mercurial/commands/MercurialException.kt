05488fa15cf543a58ceb77b102f24b8f961d46ec
Oscar Chevalier <oscar.chevalier@gmail.com>
2025-04-26 13:04:19 +1000

    feat: removed old mercurial

--- src/main/kotlin/dev/frozenmilk/mercurial/commands/MercurialException.kt
+++
@@ -1,3 +0,0 @@
-package dev.frozenmilk.mercurial.commands
-
-class MercurialException(override val message: String, override val cause: Throwable) : RuntimeException()
--- src/main/kotlin/dev/frozenmilk/mercurial/commands/StackUnwinder.kt
+++
@@ -1,6 +0,0 @@
-package dev.frozenmilk.mercurial.commands
-
-@FunctionalInterface
-fun interface StackUnwinder {
-	fun unwindStack(command: Command, substitutedSymbol: String): String
-}
