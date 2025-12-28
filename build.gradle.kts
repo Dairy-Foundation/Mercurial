446a7b7e17efa4943e3bba2748d6756c95b5871b
Oscar Chevalier <oscar@frozenmilk.dev>
2025-12-29 09:37:08 +1100

    chore: update gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	id("dev.frozenmilk.jvm-library") version "10.3.0-0.1.4"
+	id("dev.frozenmilk.jvm-library") version "11.0.0-1.0.0"
 	id("dev.frozenmilk.publish") version "0.0.5"
 	id("dev.frozenmilk.doc") version "0.0.5"
 	id("dev.frozenmilk.build-meta-data") version "0.0.2"
@@ -10,6 +10,10 @@ repositories {
 		name = "dairyReleases"
 		url = uri("https://repo.dairy.foundation/releases")
 	}
+}
+
+ftc {
+    kotlin()
 }
 
 dependencies {
